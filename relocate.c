96e3e55676a30e8579887d94a252e7fe8fb479b6
bert <bert.timmerman@xs4all.nl>
2016-01-08 20:49:24 +0100

    Included missing header files and code style modifications.
    Modified the Doxygen header.
    Added an author tag.
    Added user documentation.
    Modified coding style according to native pcb style.
    Invoke action in pcb with ":Relocate()" with a capital "R".
    
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- relocate.c
+++ relocate.c
@@ -1,9 +1,14 @@
-/** 
- * Relocate plug-in for PCB
+/*!
+ * \file src/relocate.c
+ *
+ * \brief Relocate plug-in for PCB.
+ *
+ * \author Jean Walker <jean@geemoo.ca>
  *
  * Licensed under the terms of the GNU General Public License, version
  * 2 or later.
-*/
+ */
+
 
 #include <stdio.h>
 
@@ -15,58 +20,84 @@
 #include "create.h"
 #include "rtree.h"
 #include "undo.h"
+#include "move.h"
+#include "draw.h"
+#include "set.h"
 
 
-static int relocate (int argc, char **argv, Coord x, Coord y)
+/* %start-doc actions Relocate
+
+The @code{Relocate()} action in pcb that will take all selected elements
+and stack them up at the current crosshairs.
+This works well when starting a new layout and you have hundreds of
+parts on your workspace and you're just starting to do your component
+placement.
+Use Select Element by Name (refdes) to select the elements you want to
+move, and then use :Relocate() to set the position of those elements to
+where ever your crosshair is.
+
+TODO: Right now, if multiple elements are selected, they are all are
+stacked up on top of each other.
+Instead make them spread out the way Disperse Elements does.
+
+
+%end-doc */
+static int
+relocate (int argc, char **argv, Coord x, Coord y)
 {
-	Coord origin_x, origin_y;
-	Coord dx, dy;
+  Coord origin_x;
+  Coord origin_y;
+  Coord dx;
+  Coord dy;
 
-	// get crosshair location and make this our "origin"
-	origin_x = Crosshair.X;
-	origin_y = Crosshair.Y;
+  /* get crosshair location and make this our "origin". */
+  origin_x = Crosshair.X;
+  origin_y = Crosshair.Y;
 
-	// loop over all elements, and move selected elements to crosshair location
-	ELEMENT_LOOP (PCB->Data);
-	{
-		// only affect selected elements
-		if (TEST_FLAG (SELECTEDFLAG, element)) {
-		
-			// how far does the element need to move?
-			dx = origin_x - element->BoundingBox.X1;
-			dy = origin_y - element->BoundingBox.Y1;
+  /* loop over all elements, and move selected elements to crosshair
+   * location. */
+  ELEMENT_LOOP (PCB->Data);
+  {
+    /* only affect selected elements. */
+      if (TEST_FLAG (SELECTEDFLAG, element))
+      {
+        /* how far does the element need to move? */
+        dx = origin_x - element->BoundingBox.X1;
+        dy = origin_y - element->BoundingBox.Y1;
 
-			// snap to grid
-			dx -= (element->MarkX + dx) % PCB->Grid;
-			dx += PCB->Grid;
-			dy -= (element->MarkY + dy) % PCB->Grid;
-			dy += PCB->Grid;
+        /* snap to grid. */
+        dx -= (element->MarkX + dx) % PCB->Grid;
+        dx += PCB->Grid;
+        dy -= (element->MarkY + dy) % PCB->Grid;
+        dy += PCB->Grid;
 
-			// move the element
-			MoveElementLowLevel(PCB->Data, element, dx, dy);
+        /* move the element. */
+        MoveElementLowLevel (PCB->Data, element, dx, dy);
 
-			// add to the undo list
-			AddObjectToMoveUndoList(ELEMENT_TYPE, NULL, NULL, element, dx, dy);
-		}
-	}
-	END_LOOP;
-	
-	// done with our action, so increment the undo #
-	IncrementUndoSerialNumber();
+        /* add to the undo list. */
+        AddObjectToMoveUndoList (ELEMENT_TYPE, NULL, NULL, element, dx, dy);
+      }
+  }
+  END_LOOP;
 
-	Redraw();
-	SetChangedFlag(true);
+  /* done with our action, so increment the undo # .*/
+  IncrementUndoSerialNumber ();
 
-	return 0;
+  Redraw ();
+  SetChangedFlag (true);
+
+  return 0;
 }
 
-static HID_Action relocate_action_list[] = {
-	{ "Relocate", NULL, relocate, NULL, NULL}
+
+static HID_Action relocate_action_list[] =
+{
+  { "Relocate", NULL, relocate, NULL, NULL}
 };
 
 REGISTER_ACTIONS(relocate_action_list)
 
 void hid_relocate_init()
 {
-	register_relocate_action_list();
+  register_relocate_action_list();
 }
